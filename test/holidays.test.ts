27519fecb3301fc0d33d9c5f4f05c647335875a6
Martijn Schimmel <martijn@productip>
2021-04-25 11:26:13 +0200

    Added some basic test to make sure all function are available

--- test/holidays.test.ts
+++ test/holidays.test.ts
@@ -2,16 +2,77 @@ import {Holidays} from "../src/lib/holidays";
 
 describe('Holidays', () => {
 
-  let holidays: Holidays;
+    let holidays: Holidays;
 
-  beforeEach(() => {
-    holidays = new Holidays()
-  });
+    beforeEach(() => {
+        holidays = new Holidays()
+    });
 
-  it('create', () => {
+    it('create', () => {
+        expect(holidays).toBeInstanceOf(Holidays);
+    });
 
-    expect(holidays).toBeInstanceOf(Holidays);
-  });
 
+    it('goodFriday', () => {
+
+        const date = holidays.goodFriday();
+        expect(date).toBeInstanceOf(Date);
+    });
+
+    it('bevrijdingsdag', () => {
+
+        const date = holidays.bevrijdingsdag();
+        expect(date).toBeInstanceOf(Date);
+    });
+
+    it('liberationDay', () => {
+
+        const date = holidays.liberationDay();
+        expect(date).toBeInstanceOf(Date);
+    });
+
+    it('koningsdag', () => {
+
+        const date = holidays.koningsdag();
+        expect(date).toBeInstanceOf(Date);
+    });
+
+    it('kingsday', () => {
+
+        const date = holidays.kingsDay();
+        expect(date).toBeInstanceOf(Date);
+    });
+
+    it('pinksteren', () => {
+
+        const dates = holidays.pinksteren();
+        expect(dates).toBeInstanceOf(Array);
+    });
+
+    it('pentecost', () => {
+
+        const dates = holidays.pentecost();
+        expect(dates).toBeInstanceOf(Array);
+    });
+
+    it('pasen', () => {
+
+        const dates = holidays.pasen();
+        expect(dates).toBeInstanceOf(Array);
+    });
+
+    it('easter', () => {
+
+        const dates = holidays.easter();
+        expect(dates).toBeInstanceOf(Array);
+    });
+
+    it('setYear', () => {
+
+        const entity = new Holidays();
+        entity.year = 2022;
+
+        expect(holidays.year).toBe(2022);
+    })
 
 });
