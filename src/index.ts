50f27db8a9a7b4d3bebe02f6229493ac9f509108
Martijn Schimmel <martijn@productip>
2021-04-29 19:51:36 +0200

    Added documentation

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,4 @@
 import {Holidays} from "./lib/holidays";
-import {format} from 'date-fns';
 
 
-const h = new Holidays(2026);
-
-
-
-
-h.common().forEach((d) => {
-    console.table(format(d, "yyyy-MM-dd"))
-})
+export {Holidays};
